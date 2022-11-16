0927029a08cc0d39a593a9d3dd83ef10a94ec88d
Stolyar2011 <90955514+Stolyar2011@users.noreply.github.com>
2022-11-16 17:16:56 +0300

    wishlists main page

--- wishes/templates/registration/x.py
+++ wishes/templates/registration/x.py
@@ -1,1 +1,6 @@
-print({{ form.as_p }})
+<p>
+                        <input type="text" name="username" autofocus autocapitalize="none" autocomplete="username" maxlength="150" required id="id_username" placeholder="Login">
+                    </p>
+                    <p>
+                        <input type="password" name="password" autocomplete="current-password" required id="id_password" placeholder="Password">
+                    </p>
